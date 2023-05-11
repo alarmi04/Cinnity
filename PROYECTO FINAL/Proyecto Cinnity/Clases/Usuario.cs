e6df4c3252d13b170a1adf650bedc476508eeb7b
alarmi04 <114494727+alarmi04@users.noreply.github.com>
2023-05-11 19:26:09 +0200

    Página del perfil

--- PROYECTO FINAL/Proyecto Cinnity/Clases/Usuario.cs
+++ PROYECTO FINAL/Proyecto Cinnity/Clases/Usuario.cs
@@ -8,5 +8,21 @@ namespace Proyecto_Cinnity
 {
     class Usuario
     {
+        private string nombre;
+        private string apellidos;
+        private string correo;
+        private string contrasenya;
+        private string pais;
+        private DateTime fechaNaci;
+
+        public Usuario(string nom, string ape, string corr, string clave, string pa, DateTime fecha)
+        {
+            nombre = nom;
+            apellidos = ape;
+            correo = corr;
+            contrasenya = clave;
+            pais = pa;
+            fechaNaci = fecha;
+        }
     }
 }
