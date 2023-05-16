2dc5a6a9e9f1d7fe6c8efb5ae201c7175a44fe3c
alarmi04 <114494727+alarmi04@users.noreply.github.com>
2023-05-16 16:37:56 +0200

    Cambios

--- PROYECTO FINAL/Proyecto Cinnity/Properties/Resources.Designer.cs
+++ PROYECTO FINAL/Proyecto Cinnity/Properties/Resources.Designer.cs
@@ -153,6 +153,16 @@ namespace Proyecto_Cinnity.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap configuraciones {
+            get {
+                object obj = ResourceManager.GetObject("configuraciones", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap facebook {
             get {
                 object obj = ResourceManager.GetObject("facebook", resourceCulture);
